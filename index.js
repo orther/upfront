57e144056b86bc13ae3cc97e589e01b046801727
Brandon Orther <an.able.coder@gmail.com>
2012-09-30 00:49:25 -0700

    fixed entry point and package dependencies

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = null
+var UpfrontCompiler;
+
+module.exports = UpfrontCompiler = (function() {
+
+  UpfrontCompiler.name = 'UpfrontCompiler';
+
+  function UpfrontCompiler() {}
+
+  UpfrontCompiler.prototype.brunchPlugin = false;
+
+  return UpfrontCompiler;
+
+})();
